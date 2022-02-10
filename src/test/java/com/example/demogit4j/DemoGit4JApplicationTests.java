4353f6efb90b7b9dd670065878a76a091d93ee61
IENU <ienulescu.alexandru@gmail.com>
2022-02-10 14:24:14 +0200

    Initial commit

---
+++ src/test/java/com/example/demogit4j/DemoGit4JApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demogit4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGit4JApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
